1a50ab5e9a198ac305870e9451072cd0e4995faf
Erika <erika.maust@gmail.com>
2020-01-12 22:15:45 -0800

    User registration form re-created and functioning

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,9 @@
 import datetime
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 from main.models import Image
-from main.models import Users
 
 
 class UploadForm(forms.ModelForm):
@@ -13,9 +13,9 @@ class UploadForm(forms.ModelForm):
 
 form = UploadForm()
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=False)
 
-    class meta:
-        model = Users
+    class Meta:
+        model = User
         fields = ['username', 'email', 'password1', 'password2']
