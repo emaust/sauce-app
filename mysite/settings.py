ddc4632fa3e904067a61801a2ade4d981a9a0d99
Erika <erika.maust@gmail.com>
2020-01-15 20:24:45 -0800

    Changed hyphens to underscore in import dj-database-url

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,7 +14,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 import django_heroku
-import dj-database-url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
